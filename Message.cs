f827e66dcaabc3dc15d308bd4c707e696f37396f
MinDuck <ducvominh@suctremmt.com>
2025-11-17 19:48:06 +0700

    sửa logic các class

---
+++ Message.cs
@@ -0,0 +1,10 @@
+﻿namespace APP_DOAN 
+{
+    public class Message
+    {
+        public string SenderUid { get; set; }    // UID của người gửi
+        public string SenderName { get; set; }   // Tên của người gửi
+        public string Text { get; set; }         // Nội dung tin nhắn
+        public long Timestamp { get; set; }      // Dấu thời gian (Unix)
+    }
+}
