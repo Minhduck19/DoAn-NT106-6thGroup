e848b3400fe9b762d189fa8785311c68cfcfa819
MinDuck <ducvominh@suctremmt.com>
2025-11-25 08:09:45 +0700

    sua xung dot main

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿namespace APP_DOAN
+{
+    public class User
+    {
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Lop { get; set; }
+        public string MSSV { get; set; }
+        public string NganhHoc { get; set; }
+        public string NgaySinh { get; set; }
+
+        public string MaGiangVien { get; set; }
+        public string Khoa { get; set; }
+        public string ChucVu { get; set; }
+    }
+}
