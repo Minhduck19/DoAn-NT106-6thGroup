967b39b31122c13e1a67d4738fbda7afcbc1fbad
24520338-maker <24520338@gm.uit.edu.vn>
2025-12-26 13:17:20 +0700

    Fix Nộp Bài

---
+++ GiaoDienChinh/Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_DOAN.GiaoDienChinh
+{
+    public partial class Assignment : Form
+    {
+        public Assignment()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
