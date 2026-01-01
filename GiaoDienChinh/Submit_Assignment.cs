f680ca20f8c41e27be3f57b33553d13c46915666
24520338-maker <24520338@gm.uit.edu.vn>
2026-01-01 22:13:15 +0700

    xung đột

---
+++ GiaoDienChinh/Submit_Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_DOAN.GiaoDienChinh
+{
+    public partial class Submit_Assignment : Form
+    {
+        public Submit_Assignment()
+        {
+            InitializeComponent();
+        }
+    }
+}
